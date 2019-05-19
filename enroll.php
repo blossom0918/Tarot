8ce04a28950a1208ebb4828aef5d2b15ffa7fbc8
blossom0918 <39674227+blossom0918@users.noreply.github.com>
2019-05-19 17:34:13 +0800

    Update enroll.php

--- enroll.php
+++ enroll.php
@@ -20,10 +20,10 @@
 				$newage=$_POST['age'];
 
 				$link=mysqli_connect(
-				    '192.168.0.17',
+				    	'192.168.0.17',
 					'root',
 					'sea11223',
-				    'tarot');
+				    	'tarot');
 				mysqli_select_db($link,'tarot');
 				mysqli_query($link, 'SET NAMES utf8');
 
